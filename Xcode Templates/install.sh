0cc8b16f26948e02ff0a736cf540034ef8cf6c06
Harlan Kellaway <harlan@glorify-app.com>
2023-12-21 16:31:09 -0500

    Replace CocoaPods with SPM

--- Xcode Templates/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-mkdir -p ~/"Library/Developer/Xcode/Templates/File Templates/"
-
-
-echo "$DIR/File Templates/" "-->" ~/"Library/Developer/Xcode/Templates/File Templates/"
-cp -R "$DIR/File Templates/" ~/"Library/Developer/Xcode/Templates/File Templates/"
