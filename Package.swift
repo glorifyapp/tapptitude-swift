d2a031666b6f024f4729dd7b8207976ce3a3d4dd
Harlan Kellaway <harlan@glorify-app.com>
2024-07-29 19:59:44 -0400

    Explicitly declare xibs

--- Package.swift
+++ Package.swift
@@ -12,6 +12,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "Tapptitude"),
+        .target(
+            name: "Tapptitude",
+            dependencies: [],
+            resources: [
+                .process("CollectionFeedController.xib"),
+                .process("LoadMoreView.xib")
+            ]
+        )
     ]
 )
